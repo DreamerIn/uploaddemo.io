d2f69c2582b4bf96f0a8265dd42074629c620a06
DreamerIn <chetubathi07@gmail.com>
2020-04-12 01:54:58 +0530

    Updates

---
+++ precache-manifest.6dc86965e1e08fd6bb9369885fc85fd3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60a6b79538a46df0946e66a5d440728a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40ad1d324c34a9cd4b48",
+    "url": "/static/css/2.af452921.chunk.css"
+  },
+  {
+    "revision": "77cbfbfd9e2c588d4833",
+    "url": "/static/css/main.1f758cc3.chunk.css"
+  },
+  {
+    "revision": "40ad1d324c34a9cd4b48",
+    "url": "/static/js/2.f5298358.chunk.js"
+  },
+  {
+    "revision": "8ae8dcd1d63ad08c955dfd4e6a265dcb",
+    "url": "/static/js/2.f5298358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77cbfbfd9e2c588d4833",
+    "url": "/static/js/main.78721857.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
